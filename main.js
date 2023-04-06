f99df52ea8a75b784891e5216880918de90080c7
Curtis.Lovrak <curtislovrak@gmail.com>
2023-04-05 21:50:55 -0400

    it works

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log('this is a popup!')
+console.log('this is a popup!');
+
+function getQuote() {
+    return fetch('https://type.fit/api/quotes').then(response => {
+      return response.json()
+    }).then(jsonResponse => {
+      if (!jsonResponse.length) {
+        return []
+      }
+      return jsonResponse[Math.floor(Math.random() * jsonResponse.length)]
+    })
+  }
+  getQuote().then(quote => {
+    const famousQ = document.createElement('div')
+    quoteHere.appendChild(famousQ)
+    const text = quote.text;
+    const author = quote.author;
+    // alert("quote "+ text + ", " + author)
+    console.log(text, author)
+    const display = document.getElementById('header1')
+    console.log(display)
+    display.innerHTML = (`"${text}" -${author}`)
+  })
+  
